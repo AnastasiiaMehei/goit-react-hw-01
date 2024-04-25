3fa621b8cbe77c1053201b461c1b5bfc4c45e5d9
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-04-26 00:53:13 +0300

    m,njkjj

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,17 +1,21 @@
 import Profile from "../Profile/Profile";
 import userData from "../../data/userData.json";
-function App() {
-  return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-    </>
-  )
-}
+import FriendList from '../FriendList/FriendList'
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+const App = () => {
+    return (
+      <>
+        <Profile
+          name={userData.username}
+          tag={userData.tag}
+          location={userData.location}
+          image={userData.avatar}
+          stats={userData.stats}
+        />
+        <FriendList friends={FriendListItem}/>
+      </>
+    );
+  };
 
 export default App
---
+++ src/component/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,7 @@
+export default function FriendListItem({friend:{avatar, name, isOnline}}){
+    return(<div>
+        <img src={avatar} alt="Avatar" width="48" />
+        <p>Friend name: {name}</p>
+        <p>Friend status: {isOnline}</p>
+      </div>)
+}
