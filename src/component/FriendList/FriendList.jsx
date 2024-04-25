7f664e051543562107eabef5cde3863fb1fe7caf
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-04-26 01:13:00 +0300

    kljjl

--- src/component/FriendList/FriendList.jsx
+++ src/component/FriendList/FriendList.jsx
@@ -1,6 +1,6 @@
 // import css from './FriendList.module.css';
 import FriendListItem from '../FriendListItem/FriendListItem';
-export default function FriendList(friends){
+export default function FriendList({friends}){
     return(<ul>
        {friends.map((friend)=><li key={friend.id}>
             <FriendListItem friend={friend} />
