4c206242bd031716b3b7903b3a9743a8267f169c
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-04-26 11:34:25 +0300

    add

--- src/component/FriendList/FriendList.jsx
+++ src/component/FriendList/FriendList.jsx
@@ -1,7 +1,7 @@
-// import css from './FriendList.module.css';
+import css from './FriendList.module.css'
 import FriendListItem from '../FriendListItem/FriendListItem';
 export default function FriendList({friends}){
-    return(<ul>
+    return(<ul className={css.list}>
        {friends.map((friend)=><li key={friend.id}>
             <FriendListItem friend={friend} />
         </li>)}
--- src/component/FriendListItem/FriendListItem.jsx
+++ src/component/FriendListItem/FriendListItem.jsx
@@ -1,7 +1,14 @@
+import clsx from 'clsx'
+import css from './FriendListItem.module.css'
 export default function FriendListItem({friend:{avatar, name, isOnline}}){
-    return(<div>
+    return(<div className={css.items}>
         <img src={avatar} alt="Avatar" width="48" />
-        <p>Friend name: {name}</p>
-        <p>Friend status: {isOnline}</p>
+        <p>{name}</p>
+        <p className={clsx({
+        [css.online]: isOnline,
+        [css.offline]: !isOnline,
+      })}>
+        {isOnline ? "online" : "offline"}
+      </p>
       </div>)
 }
